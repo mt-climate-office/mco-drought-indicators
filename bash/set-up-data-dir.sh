07da2e3891cc85a24ddcab10f5ee8da0d8c6b67d
Zach Hoylman <zhoylman@gmail.com>
2021-02-18 12:07:26 -0700

    initial commit

---
+++ bash/set-up-data-dir.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+mkdir /home/zhoylman/mco-drought-indicators-data/
+mkdir /home/zhoylman/mco-drought-indicators-data/spi
+mkdir /home/zhoylman/mco-drought-indicators-data/spei
+mkdir /home/zhoylman/mco-drought-indicators-data/eddi
+mkdir /home/zhoylman/mco-drought-indicators-data/precipitation
+mkdir /home/zhoylman/mco-drought-indicators-data/temperature
+mkdir /home/zhoylman/mco-drought-indicators-data/snodas
+mkdir /home/zhoylman/mco-drought-indicators-data/snodas/processed
+mkdir /home/zhoylman/mco-drought-indicators-data/snodas/raw
+mkdir /home/zhoylman/mco-drought-indicators-data/snodas/processed/delta_snow_depth
+mkdir /home/zhoylman/mco-drought-indicators-data/snodas/processed/snow_depth
+mkdir /home/zhoylman/mco-drought-indicators-data/snodas/processed/swe
+mkdir /home/zhoylman/mco-drought-indicators-data/ndvi
+mkdir /home/zhoylman/mco-drought-indicators-data/ndvi/temp
+
