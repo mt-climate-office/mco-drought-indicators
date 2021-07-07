489d81811bc555f12c2f25df4e47b80c83f2bdd6
zhoylman <zhoylman@gmail.com>
2021-07-07 13:22:32 -0600

    updating index.html

--- bash/install-R-libs.sh
+++ bash/install-R-libs.sh
@@ -35,3 +35,4 @@ sudo su - -c "R -q -e \"install.packages('RNRCS', repos='http://cran.rstudio.com
 sudo su - -c "R -q -e \"install.packages('tictoc', repos='http://cran.rstudio.com/')\""
 sudo su - -c "R -q -e \"install.packages('data.table', repos='http://cran.rstudio.com/')\""
 sudo su - -c "R -q -e \"install.packages('ggplot2', repos='http://cran.rstudio.com/')\""
+sudo su - -c "R -q -e \"install.packages('flexdashboard', repos='http://cran.rstudio.com/')\""
