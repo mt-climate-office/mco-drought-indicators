1f091341fc2455643739cc403a83bd166807391b
zhoylman <zhoylman@gmail.com>
2021-11-01 15:45:18 -0600

    updated mesonet anomoly computation

--- bash/rebuild-maps.sh
+++ bash/rebuild-maps.sh
@@ -4,6 +4,7 @@ Rscript /home/zhoylman/mco-drought-indicators/processing/build-widgets/R/build-h
 Rscript /home/zhoylman/mco-drought-indicators/processing/build-widgets/R/build-html-precip-temp.R
 Rscript /home/zhoylman/mco-drought-indicators/processing/build-widgets/R/build-html-cpc-and-grace.R
 Rscript /home/zhoylman/mco-drought-indicators/processing/build-widgets/R/build-html-snotel-snodas.R 
-Rscript /home/zhoylman/mco-drought-indicators/processing/build-widgets/R/build-html-ndvi.R   
+Rscript /home/zhoylman/mco-drought-indicators/processing/build-widgets/R/build-html-ndvi.R  
+Rscript /home/zhoylman/mco-drought-indicators/processing/build-widgets/R/build-html-smap.R >/home/zhoylman/mco-drought-indicators-data/logs/build-html-smap 2>&1
 
 cp -r /home/zhoylman/mco-drought-indicators-data/widgets/ /var/data/drought-indicators/
--- processing/compute-point/mesonet/soil-moisture-anomoly.R
+++ processing/compute-point/mesonet/soil-moisture-anomoly.R
@@ -1,6 +1,7 @@
-library(tidyverse)
+library(dplyr)
 library(RCurl)
 library(lubridate)
+library(readr)
 
 `%notin%` = Negate(`%in%`)
 
@@ -18,13 +19,35 @@ stations = getURL("https://mesonet.climate.umt.edu/api/stations?type=csv&clean=t
   dplyr::select(`Station ID`, Longitude, Latitude, `Station name`) %>%
   rename(station_key = `Station ID`)
 
+#compute days of interest (and adjust for day changes over a year or under)
+today = Sys.Date()
+#today = as.Date('2021-11-25')
+today_yday = lubridate::yday(today)
+min_yday = lubridate::yday(today-15)
+max_yday =  lubridate::yday(today+15)
+
+if(today_yday + 15 > 366){
+  max_day_seq = c((today_yday:366), (1:max_yday))
+} else {
+  max_day_seq = today_yday:max_yday
+}
+
+if(today_yday - 15 < 1){
+  min_day_seq = c(min_yday:366, 1:today_yday)
+} else {
+  min_day_seq = min_yday:today_yday
+}
+days_of_interest = c(min_day_seq, max_day_seq) %>% unique()
+
 anom = data %>%
-  mutate(hour = hour(datetime)) %>%
+  mutate(hour = hour(datetime),
+         minute = minute(datetime)) %>%
   filter(hour == 0,
+         minute == 0,
          name %in% c("soilwc00", "soilwc04", "soilwc08", "soilwc20", "soilwc36"),
          station_key %notin% c('whitshaw', 'mdaglasw', 'blmroyno')) %>%
-  mutate(week = lubridate::week(datetime)) %>%
-  filter(week %in% c(lubridate::week(Sys.Date()),lubridate::week(Sys.Date())-1))%>%
+  mutate(yday = lubridate::yday(datetime)) %>%
+  filter(yday %in% days_of_interest)%>%
   group_by(station_key, name) %>%
   mutate(anom = gamma_fit_spi(value, return_latest = F))%>%
   ungroup() %>%
