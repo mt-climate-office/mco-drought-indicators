ae27277c0700c340731a18037852028acae48ab8
zhoylman <zhoylman@gmail.com>
2022-05-02 14:34:02 -0600

    merge

--- processing/cog/R/16bit-rescaling.R
+++ processing/cog/R/16bit-rescaling.R
@@ -25,3 +25,66 @@ for(i in 1:length(names)){
   writeRaster(data[[i]], paste0('/home/zhoylman/mco-drought-indicators-data/16bit-rescaled/', names[i]), datatype='INT2S',  overwrite=TRUE, NAflag = -32767)
 }
 
+
+#precipititaion 
+
+names = list.files('/home/zhoylman/mco-drought-indicators-data/precipitation', full.names = F, pattern = "*.tif") 
+
+data = list.files('/home/zhoylman/mco-drought-indicators-data/precipitation', full.names = T, pattern = "*.tif") %>%
+  lapply(., rescale)
+
+for(i in 1:length(names)){
+  writeRaster(data[[i]], paste0('/home/zhoylman/mco-drought-indicators-data/16bit-rescaled/precipitation_', names[i]), datatype='INT2S',  overwrite=TRUE, NAflag = -32767)
+}
+
+#temperature 
+
+names = list.files('/home/zhoylman/mco-drought-indicators-data/temperature', full.names = F, pattern = "*.tif") 
+
+data = list.files('/home/zhoylman/mco-drought-indicators-data/temperature', full.names = T, pattern = "*.tif") %>%
+  lapply(., rescale)
+
+for(i in 1:length(names)){
+  writeRaster(data[[i]], paste0('/home/zhoylman/mco-drought-indicators-data/16bit-rescaled/temperature_', names[i]), datatype='INT2S',  overwrite=TRUE, NAflag = -32767)
+}
+
+#Soil Moisture 
+
+names = list.files('/home/zhoylman/mco-drought-indicators-data/smap/data', full.names = F, pattern = "*.tif") 
+
+data = list.files('/home/zhoylman/mco-drought-indicators-data/smap/data', full.names = T, pattern = "*.tif")  %>%
+  lapply(., rescale)
+
+for(i in 1:length(names)){
+  writeRaster(data[[i]], paste0('/home/zhoylman/mco-drought-indicators-data/16bit-rescaled/', names[i]), datatype='INT2S',  overwrite=TRUE, NAflag = -32767)
+}
+
+
+#GRACE groundwater
+
+names = list.files('/home/zhoylman/mco-drought-indicators-data/grace', full.names = F, pattern = "*.tif") 
+
+data = list.files('/home/zhoylman/mco-drought-indicators-data/grace', full.names = T, pattern = "*.tif")  %>%
+  lapply(., rescale)
+
+for(i in 1:length(names)){
+  writeRaster(data[[i]], paste0('/home/zhoylman/mco-drought-indicators-data/16bit-rescaled/', names[i]), datatype='INT2S',  overwrite=TRUE, NAflag = -32767)
+}
+
+#NDVI
+
+names = list.files('/home/zhoylman/mco-drought-indicators-data/ndvi/data/anom', full.names = F, pattern = "*.tif") %>%
+  as_tibble() %>%
+  bind_rows(as_tibble(list.files('/home/zhoylman/mco-drought-indicators-data/ndvi/data/trend', full.names = F, pattern = "*.tif"))) %$%
+  value
+
+data = list.files('/home/zhoylman/mco-drought-indicators-data/ndvi/data/anom', full.names = T, pattern = "*.tif") %>%
+  as_tibble() %>%
+  bind_rows(as_tibble(list.files('/home/zhoylman/mco-drought-indicators-data/ndvi/data/trend', full.names = T, pattern = "*.tif"))) %$%
+  value %>%
+  lapply(., rescale)
+
+for(i in 1:length(names)){
+  writeRaster(data[[i]], paste0('/home/zhoylman/mco-drought-indicators-data/16bit-rescaled/', names[i]), datatype='INT2S',  overwrite=TRUE, NAflag = -32767)
+}
+
