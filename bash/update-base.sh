564bf3c5961883f030cded4b1ec41ad2f72afea2
zhoylman <zhoylman@gmail.com>
2023-06-20 11:23:38 -0600

    add vhi processing to the bash

--- bash/update-base.sh
+++ bash/update-base.sh
@@ -9,6 +9,7 @@ Rscript /home/zhoylman/mco-drought-indicators/processing/compute-spatial/drought
 Rscript /home/zhoylman/mco-drought-indicators/processing/compute-spatial/drought-metrics/R/temperature-map.R >/home/zhoylman/mco-drought-indicators-data/logs/temp 2>&1
 Rscript /home/zhoylman/mco-drought-indicators/processing/compute-spatial/drought-metrics/R/get-cpc-and-grace.R >/home/zhoylman/mco-drought-indicators-data/logs/cpc-grace 2>&1
 Rscript /home/zhoylman/mco-drought-indicators/processing/compute-point/mesonet/soil-moisture-anomoly.R >/home/zhoylman/mco-drought-indicators-data/logs/mesonet-anom 2>&1
+Rscript /home/zhoylman/mco-drought-indicators/processing/compute-spatial/drought-metrics/R/process-vhi.R >/home/zhoylman/mco-drought-indicators-data/logs/vhi 2>&1
 
 # Update Snotel Data
 #Rscript /home/zhoylman/mco-drought-indicators/processing/compute-point/snotel/R/compute-current-anomaly.R >/home/zhoylman/mco-drought-indicators-data/logs/download-snotel 2>&1
