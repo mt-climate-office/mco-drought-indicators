27954e513a5b0ebaa9d0abd5eb7640805f5e1352
Zach Hoylman <zhoylman@gmail.com>
2025-10-26 17:53:47 -0600

    updated snodas

--- processing/ancillary-functions/R/get-snodas.R
+++ processing/ancillary-functions/R/get-snodas.R
@@ -1,47 +1,80 @@
-# snodas ingestion script and conversion to GeoTiff
-# Zach Hoylman 2-18-2021
-
-# snodas help info
-# help https://nsidc.org/support/how/how-do-i-convert-snodas-binary-files-geotiff-or-netcdf
-# naming table 4 https://nsidc.org/data/g02158#untar_daily_nc
-# data ftp://sidads.colorado.edu/DATASETS/NOAA/G02158/
+# SNODAS ingestion and conversion to GeoTIFF
+# Updated by Zach Hoylman on 10-26-2025
 
 library(httr)
 library(dplyr)
 library(data.table)
+library(R.utils)
 
-export.dir = '/home/zhoylman/mco-drought-indicators-data/'
+export.dir = "/home/zhoylman/mco-drought-indicators-data/"
 
-get_snodas = function(date){
-  for(d in 1:length(date)){
+get_snodas = function(date) {
+  
+  for (d in seq_along(date)) {
+    message("---- Processing ", format(date[d], "%Y-%m-%d"), " ----")
+    
     tryCatch({
-      #build data url
-      url = paste0("ftp://sidads.colorado.edu/DATASETS/NOAA/G02158/masked/", 
-                   date[d] %>% format(., "%Y"), "/", date[d] %>% format(., "%m_%b"), 
-                   "/SNODAS_", date[d] %>% format(., "%Y%m%d"), ".tar")
+      # ---- Ensure directory structure exists ----
+      dirs_to_check = c(
+        paste0(export.dir, "snodas/"),
+        paste0(export.dir, "snodas/raw/"),
+        paste0(export.dir, "snodas/processed/"),
+        paste0(export.dir, "snodas/processed/swe/"),
+        paste0(export.dir, "snodas/processed/snow_depth/")
+      )
+      lapply(dirs_to_check, function(x) if (!dir.exists(x)) dir.create(x, recursive = TRUE))
       
-      #define where raw tarball will be stored
-      tar.dir = paste0(export.dir,"snodas/raw/SNODAS_", date[d] %>% format(., "%Y%m%d"), ".tar")
-      unzip.dir = paste0(export.dir, "snodas/raw/SNODAS_", date[d] %>% format(., "%Y%m%d"))
+      # ---- Build URL and paths ----
+      url = paste0(
+        "https://noaadata.apps.nsidc.org/NOAA/G02158/masked/",
+        format(date[d], "%Y"), "/", format(date[d], "%m_%b"),
+        "/SNODAS_", format(date[d], "%Y%m%d"), ".tar"
+      )
       
-      #downlaod zipped data
-      httr::GET(url, write_disk(path = tar.dir, overwrite=TRUE))
+      tar.dir = path.expand(paste0(export.dir, "snodas/raw/SNODAS_", format(date[d], "%Y%m%d"), ".tar"))
+      unzip.dir = path.expand(paste0(export.dir, "snodas/raw/SNODAS_", format(date[d], "%Y%m%d")))
       
-      #create unzip location
-      dir.create(unzip.dir)
+      # ---- Skip if processed files already exist ----
+      swe_out = paste0(export.dir, "snodas/processed/swe/snodas_swe_conus_", format(date[d], "%Y%m%d"), ".tif")
+      depth_out = paste0(export.dir, "snodas/processed/snow_depth/snodas_snow_depth_conus_", format(date[d], "%Y%m%d"), ".tif")
+      if (file.exists(swe_out) && file.exists(depth_out)) {
+        message("Files already processed for ", format(date[d], "%Y-%m-%d"), ", skipping.")
+        next
+      }
       
-      #unzip file
-      untar(tarfile = tar.dir,  exdir = unzip.dir)
+      # ---- Download file with verification ----
+      res = httr::GET(url, write_disk(tar.dir, overwrite = TRUE))
+      if (httr::status_code(res) != 200 || file.info(tar.dir)$size < 1e6) {
+        stop("Download failed or file too small: ", url)
+      }
       
-      #get files from unzipped dir
-      files = list.files(unzip.dir, full.names = T) 
+      # ---- Create unzip directory ----
+      if (!dir.exists(unzip.dir)) dir.create(unzip.dir)
       
-      #define NOAA IDs for variables of interest
-      files_of_interest = c("1034", "1036")
+      # ---- Unpack tarball safely ----
+      untar_success = tryCatch({
+        untar(tarfile = tar.dir, exdir = unzip.dir)
+        TRUE
+      }, warning = function(w) {
+        message("Warning during untar: ", conditionMessage(w))
+        FALSE
+      }, error = function(e) {
+        message("Error during untar: ", conditionMessage(e))
+        FALSE
+      })
       
-      for(i in 1:length(files_of_interest)) {
-        file_to_process = files[[which(files %like% files_of_interest[i] & files %like% ".dat.gz")]]
+      if (!untar_success) stop("Failed to extract tarball for ", format(date[d], "%Y-%m-%d"))
+      
+      # ---- Identify and process data files ----
+      files = list.files(unzip.dir, full.names = TRUE)
+      files_of_interest = c("1034", "1036")  # SWE and snow depth
+      
+      for (i in seq_along(files_of_interest)) {
+        file_to_process = files[grepl(files_of_interest[i], files) & grepl(".dat.gz$", files)]
+        if (length(file_to_process) == 0) next
         
+        # ---- Write ENVI header ----
+        hdr_path = sub(".dat.gz", ".hdr", file_to_process)
         writeLines(
           "ENVI
 samples = 6935
@@ -51,36 +84,46 @@ header offset = 0
 file type = ENVI Standard
 data type = 2
 interleave = bsq
-byte order = 1", con = file_to_process %>% gsub(".dat.gz", ".hdr", .))
+byte order = 1", con = hdr_path
+        )
         
-        file_to_process %>%
-          R.utils::gunzip(., destname = gsub(".gz", "", .)) 
+        # ---- Unzip binary data ----
+        gunzip(file_to_process, destname = sub(".gz", "", file_to_process), overwrite = TRUE)
         
-        processed_name = paste0(export.dir, "snodas/processed/",
-                                if(files_of_interest[i] == "1034") paste0("swe/snodas_swe_conus_", format(date[d],"%Y%m%d"), ".tif") 
-                                else if (files_of_interest[i] == "1036") paste0("snow_depth/snodas_snow_depth_conus_", format(date[d],"%Y%m%d"), ".tif") else NA)
+        # ---- Define output GeoTIFF path ----
+        processed_name = if (files_of_interest[i] == "1034") {
+          swe_out
+        } else if (files_of_interest[i] == "1036") {
+          depth_out
+        } else NA
         
-        #different file dimentions after Oct. 2013
-        if(date[d] > as.Date('2013-10-01')){
-          print('Data from after Oct. 2013')
-          system(paste0("gdal_translate -of GTiff -a_srs '+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs' -a_nodata -9999 -a_ullr  -124.73333333 52.87500000 -66.94166667 24.95000000 ",
-                        file_to_process %>% gsub(".gz", "", .), " ",
-                        processed_name))
-        }
-        else{
-          print('Data from before Oct. 2013')
-          system(paste0("gdal_translate -of GTiff -a_srs '+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs' -a_nodata -9999 -a_ullr  -124.73375000000000 52.87458333333333 -66.94208333333333 24.94958333333333 ",
-                        file_to_process %>% gsub(".gz", "", .), " ",
-                        processed_name))
+        # ---- Convert to GeoTIFF ----
+        if (date[d] > as.Date("2013-10-01")) {
+          message("Data from after Oct 2013")
+          system(paste0(
+            "gdal_translate -of GTiff -a_srs '+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs' ",
+            "-a_nodata -9999 -a_ullr -124.73333333 52.87500000 -66.94166667 24.95000000 ",
+            sub(".gz", "", file_to_process), " ", processed_name
+          ))
+        } else {
+          message("Data from before Oct 2013")
+          system(paste0(
+            "gdal_translate -of GTiff -a_srs '+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs' ",
+            "-a_nodata -9999 -a_ullr -124.73375000000000 52.87458333333333 -66.94208333333333 24.94958333333333 ",
+            sub(".gz", "", file_to_process), " ", processed_name
+          ))
         }
       }
       
-      #erase raw data so we dont over store
-      system(paste0("rm -r ", tar.dir))
-      system(paste0("rm -r ", unzip.dir))
-    }, error = function(e){
-      system(paste0("rm -r ", tar.dir))
-      system(paste0("rm -r ", unzip.dir))
+      # ---- Clean up raw files ----
+      unlink(tar.dir)
+      unlink(unzip.dir, recursive = TRUE)
+      message("Finished processing ", format(date[d], "%Y-%m-%d"))
+      
+    }, error = function(e) {
+      message("❌ Error on ", format(date[d], "%Y-%m-%d"), ": ", conditionMessage(e))
+      unlink(tar.dir)
+      unlink(unzip.dir, recursive = TRUE)
     })
   }
 }
